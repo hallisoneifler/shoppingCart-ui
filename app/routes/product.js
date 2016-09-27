a1278d2465e1b0eeeb39b5571e7d16691d0f87c1
Hallison Eifler <hallison.e@gmail.com>
2016-09-26 21:15:54 -0300

    t

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,18 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  cart: Ember.inject.service('product-service'),
   ajax: Ember.inject.service(),
   actions: {
     addItem(item) {
       let id_product = item.id;
-      let quantity = 2;
-      console.log('Values: ' + id_product + ' -- ' + quantity);
-      return this.get('ajax').request('/shoppingCart', {
-        method: 'POST'
-      }).then(()=>{
-        console.log('Final ');
-        this.transitionTo('shopping-cart');
-      });
+      this.get('cart').add(item);
     }
   },
   model() {
